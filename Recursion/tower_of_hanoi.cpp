63dcdaff3a129026267aef0dec00891c775eec7d
Priya <155173659+Priya-2212@users.noreply.github.com>
2024-10-02 19:13:03 +0530

    Update tower_of_hanoi.cpp

--- Recursion/tower_of_hanoi.cpp
+++ Recursion/tower_of_hanoi.cpp
@@ -1,15 +1,24 @@
+#include <iostream>
+using namespace std;
 
- long long toh(int n, int from, int to, int aux) {
-        // Your code here
+long long toh(int n, int from, int to, int aux) {
+        
          if(n == 1) {
-            cout << "move disk" << n << "from rod" << from << "to rod" << to << endl;
+            cout << "move disk " << n << " from rod " << from << "to rod " << to << endl;
             return 1;
         }
         int count = toh(n-1, from, aux, to);
         
-        cout << "move disk" << n << "from rod" << from <<"to rod" << to << endl;
+        cout << "move disk " << n << " from rod " << from <<"to rod " << to << endl;
         
         count += 1;
         count += toh(n-1, aux, to, from);
         return count;
-    }
+}
+int main()
+{
+    int N = 3;
+    toh(N, 'A', 'C', 'B');
+    return 0;
+}
+
