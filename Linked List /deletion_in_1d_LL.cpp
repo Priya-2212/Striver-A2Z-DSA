6e009902cdf309288010d9ff1d63a476b1f515d2
Priya <155173659+Priya-2212@users.noreply.github.com>
2025-01-01 20:46:04 +0530

    Create deletion_in_1d_LL.cpp

---
+++ Linked List /deletion_in_1d_LL.cpp
@@ -0,0 +1,11 @@
+//from a linked list, the deletion can be done in four ways: head, position, value and last
+
+// deletion of head of ll
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(Node* head) {
+while(head!=
+Node* removesHead(Node* head) {
+  
