4105c08584e39e6b216e12de5444cc9a49445270
Priya <155173659+Priya-2212@users.noreply.github.com>
2025-02-01 22:22:26 +0530

    Create 142. Linked List Cycle II.cpp

---
+++ Linked List /142. Linked List Cycle II.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+//brute force
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* temp = head;
+       
+        unordered_map<ListNode*, int> mp;
+        
+        while(temp!=NULL){
+            if(mp.count(temp)!=0){
+                return temp;
+            }
+        mp[temp] = 1;
+        temp = temp->next;
+    }
+    return nullptr; 
+    }
+};
+
+//tc ->
+//sc ->
+
+
+//optimal
